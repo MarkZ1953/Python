3188bba262a29b8fa4e7b7af02cdafcb3241238c
Felipe Castro <castrolopez.davidfelipe@gmail.com>
2023-03-27 15:34:51 -0500

    .

--- Concejos y Trucos/Funciones/FuncionesAnidadas.py
+++ Concejos y Trucos/Funciones/FuncionesAnidadas.py
@@ -12,6 +12,29 @@ def mostrar(texto):
 # se crea la funcion interna convetir_minusculas
 print(mostrar('Desde funcion anidada...'))
 
+
 # No podemos utilizar la funcion interna desde fuera de la funcion externa
 # convetir_minusculas('texto1')
 # mostrar.convertir_minusculas('texto1')
+
+# Retornar la funcion anidada
+# Observar que en ningun momento se llama la funcion anidada desde la funcion externa
+def hablar(volumen):
+    def minusculas(texto):
+        return texto.lower()
+
+    def mayusculas(texto):
+        return texto.upper()
+
+    if volumen > 0.5:
+        return mayusculas
+    else:
+        return minusculas
+
+
+# Utilizamos la funcion anidada
+print(hablar(0.6)('hablo fuerte...'))
+print(hablar(0.2)('Hablo Suave...'))
+
+variable_minusculas = hablar(0.4)
+print(variable_minusculas('Hablo Suave Nuevamente'))
